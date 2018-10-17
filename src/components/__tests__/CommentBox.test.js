5549da90ca2fa3f6e4d9c70cc476621f99be5a01
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-17 17:07:41 -0500

    initial commentBox test working

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import { mount } from 'enzyme';
 
 import CommentBox from 'components/CommentBox';
 
-it('has a text area and a button'. () => {});
+// Using Full DOM rendering for practice
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />)
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+
+it('has a text area and a button', () => {
+
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+
+});
