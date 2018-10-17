913f9c8c55850b55b65037e7ddd5605b4020448b
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-17 16:44:36 -0500

    commentBox wired up and changing comment state

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+  state = { comment: ''};
+
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(`submitting: ${this.state.comment}`);
+    // TODO: Call action creator
+    // save comment
+    this.setState({ comment: '' });
+  }
+
   render(){
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add a Comment</h4>
-        <textarea />
+        <textarea value={this.state.comment} onChange={this.handleChange}/>
         <div>
-          <button>Submit content</button>
+          <button type="submit">Submit content</button>
         </div>
       </form>
     )
