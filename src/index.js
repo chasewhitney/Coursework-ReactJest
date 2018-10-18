8cfd48eebd1bef26590f1a8ae4de054969ec0690
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-18 01:39:15 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware} from 'redux';
+import { createStore } from 'redux';
 
 import reducers from 'reducers';
 
 import App from 'components/App';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStore(reducers, {})}>
     <App />
   </Provider>,
   document.querySelector('#root')
