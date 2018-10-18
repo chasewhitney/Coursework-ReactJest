b65b975844433c535ce3c70d0647536b594344f0
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-18 14:09:15 -0500

    created CreateComment reducer test

---
+++ src/reducers/__tests__/reducers.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CommentReducer from 'reducers/reducer_CreateComment';
+import { CREATE_COMMENT } from 'actions/types';
+
+it('properly hands off actions with type CREATE_COMMENT',() => {
+  const action = {
+    type: CREATE_COMMENT,
+    payload: "new comment"
+  };
+
+  expect(CommentReducer([], action)).toEqual(["new comment"]);
+});
