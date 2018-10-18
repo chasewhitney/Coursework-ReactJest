20d677bebbd6c49aa49c8557fb31b2b457c673fe
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-17 23:06:59 -0500

    set up structure for reducers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+
+import reducers from 'reducers';
 
 import App from 'components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
