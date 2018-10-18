5177b195bdaa01091099979a5e410665c2ea7ff4
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-17 23:54:15 -0500

    CommentList from function component to class component

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-  return <div>Comment list</div>;
+class CommentList extends Component {
+  render() {
+    return(
+      <div>Comment List</div>
+    )
+  }
 }
+
+
+export default CommentList;
