15bd82ae042e206de51ca67ba04e47cbbb8b999e
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-18 00:01:18 -0500

    connected CommentList to state, is rendering array of comments from state

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -4,10 +4,16 @@ import { connect } from 'react-redux';
 class CommentList extends Component {
   render() {
     return(
-      <div>Comment List</div>
-    )
-  }
-}
+      <div>
+        <div>Comment List</div>
+        {this.props.comments.map(comment => <p key={comment}>{comment}</p>)}
+      </div>
+    );
+  };
+};
 
+function mapPropsToState({ comments }) {
+  return { comments };
+};
 
-export default CommentList;
+export default connect(mapPropsToState)(CommentList);
